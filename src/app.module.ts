a561b12a2dc0fd7be0b9afc883234797f3a80b82
BartoszDembek <bartoszdembek944@gmail.com>
2025-02-26 20:31:22 +0100

    poprawa app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
@@ -7,6 +8,10 @@ import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     AuthModule,
     SpotifyModule,
     UserModule
